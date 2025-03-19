eff73800accd3269e8a8c7448fffec55f9c755f5
Pratik Makwana <102281511+pratikmakwana10@users.noreply.github.com>
2025-03-19 14:57:03 +0530

    skills added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2099671344"
+    serviceWorkerVersion: "2311028406"
   }
 });
